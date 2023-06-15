4e735c8de1f6a80cfc914d74429846f8ebc46cb4
Aimen Ellahi <you@example.com>
2023-06-16 03:16:59 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FirstSection from './Components/FirstSection';
+
+function App() {
+  return (
+    <div>
+      <FirstSection />
+    </div>
+  );
+}
+
+export default App;
