8e35f23bdd976eedb0e33bd5204affb7a786cd04
Hanan Munir <66138578+hananmunir@users.noreply.github.com>
2023-06-16 19:50:41 +0500

    Removed Loader

--- src/Components/FirstSection.jsx
+++ src/Components/FirstSection.jsx
@@ -1,69 +1,69 @@
-import React, { useRef, Suspense } from "react";
-import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { useControls } from "leva";
-import { OrbitControls, useProgress, Html } from "@react-three/drei";
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+
 import { Mascot } from "./Mascot";
-import "./index.css"
-
-
-const Loader = () => {
-  const { progress } = useProgress();
-  return (
-    
-    <Html center style={{ color: "#FFFFFF", fontSize: "5vw" ,marginTop: "20vw", marginLeft: "20vw" }}>
-      {progress}
-    </Html>
-  );
-};
+import "./index.css";
 
 const MascotModel = () => {
   return (
-    <div style={{ width: "100%", height: "80vh", backgroundColor: "#1E1E1E", float: "right", margin: "0", padding: "0", position: "relative" }}>
-      <Canvas camera={{ position: [0, 0, 0], fov: 37 }} >
+    <div
+      style={{
+        width: "100%",
+        height: "80vh",
+        backgroundColor: "#1E1E1E",
+        float: "right",
+        margin: "0",
+        padding: "0",
+        position: "relative",
+      }}
+    >
+      <Canvas camera={{ position: [0, 0, 0], fov: 37 }}>
         <ambientLight intensity={0.8} />
         <spotLight position={[10, 15, 10]} angle={0.3} />
-        <Suspense fallback={<Loader />}>
+        <Suspense fallback={null}>
           <directionalLight intensity={0.5} />
           <pointLight position={[10, 5, 10]} />
-          <Mascot/>
+          <Mascot />
         </Suspense>
       </Canvas>
     </div>
   );
-}
+};
 
 export default function Index() {
-  
   return (
-    <div className="outer-container-first" style={{ width: "100%", height: "110vh", backgroundColor: "#1E1E1E"}}>
-      <div className="inner-container-first" >
-        <div className="header-container-first">
-          <h1 className="header-first">Born</h1>
+    <div
+      className='outer-container-first'
+      style={{ width: "100%", height: "110vh", backgroundColor: "#1E1E1E" }}
+    >
+      <div className='inner-container-first'>
+        <div className='header-container-first'>
+          <h1 className='header-first'>Born</h1>
         </div>
-        <MascotModel/>
-        <div className="text-container-first">
-          <span className="meet-text">Meet BeeDee</span>
-          <div className="button-text-container">
-            <span className="text-first">
-              Get to know BeeDee more
-            </span>
-            <button className="button-first">
-              <span style={{
-                fontSize: "1.5rem",
-                }} >
-                  &rarr;
+        <MascotModel />
+        <div className='text-container-first'>
+          <span className='meet-text'>Meet BeeDee</span>
+          <div className='button-text-container'>
+            <span className='text-first'>Get to know BeeDee more</span>
+            <button className='button-first'>
+              <span
+                style={{
+                  fontSize: "1.5rem",
+                }}
+              >
+                &rarr;
               </span>
             </button>
           </div>
         </div>
-        <div className="bottom-text-container-first">
-          <div  className="star-img">
-            <img src="/Star.svg" alt="start" />
+        <div className='bottom-text-container-first'>
+          <div className='star-img'>
+            <img src='/Star.svg' alt='start' />
           </div>
-          <span className="star-text">EDEN DIGITAL MASCOTTE</span>
+          <span className='star-text'>EDEN DIGITAL MASCOTTE</span>
         </div>
       </div>
-      <div className="line-break"></div>
+      <div className='line-break'></div>
     </div>
   );
 }
