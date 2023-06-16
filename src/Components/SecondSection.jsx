5a291928fdca11a89ad6f8cbcbea7a692b4732be
Hanan Munir <66138578+hananmunir@users.noreply.github.com>
2023-06-16 15:35:04 +0500

    Fixed Margin

--- src/Components/SecondSection.jsx
+++ src/Components/SecondSection.jsx
@@ -1,37 +1,39 @@
-import React from 'react'
+import React from "react";
 
 function SecondSection() {
   return (
-    <div className='outer-container-sec' >
-        <div style={{
+    <div className='outer-container-sec'>
+      <div
+        style={{
           height: "40vh",
           width: "30%",
           marginTop: "5%",
           marginLeft: "15%",
           display: "flex",
           justifyContent: "center",
-          alignItems: "center"
-          }}>
-          <img src="/Doing.png" alt="Your Image"/>
-        </div>
+          alignItems: "center",
+        }}
+      >
+        <img src='/Doing.png' alt='Your Image' />
+      </div>
 
-         <div className="text-container-sec">
-          <span className="buddy-text">And his buddy, LooKaa</span>
-          <div className="button-text-container-sec">
-            <span className="text-sec">
-              Get to know LooKaa more
-            </span>
-            <button className="button-first">
-              <span style={{
+      <div className='text-container-sec'>
+        <span className='buddy-text'>And his buddy, LooKaa</span>
+        <div className='button-text-container-sec'>
+          <span className='text-sec'>Get to know LooKaa more</span>
+          <button className='button-first'>
+            <span
+              style={{
                 fontSize: "1.5rem",
-                }} >
-                  &rarr;
-              </span>
-            </button>
-          </div>
+              }}
+            >
+              &rarr;
+            </span>
+          </button>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SecondSection
+export default SecondSection;
